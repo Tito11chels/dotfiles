8dff7a8348e1b2d3d18e64dee9cfea108ea9419b
TakumiO <18618624+TakumiO@users.noreply.github.com>
2022-10-16 21:50:52 +0900

    conda.zshの修正

--- .zsh/conda.zsh
+++ .zsh/conda.zsh
@@ -1,13 +1,13 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('$HOME/.pyenv/versions/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/Users/takumi/.pyenv/versions/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "$HOME/.pyenv/versions/miniforge3/etc/profile.d/conda.sh" ]; then
-        . "$HOME/.pyenv/versions/miniforge3/etc/profile.d/conda.sh"
+    if [ -f "/Users/takumi/.pyenv/versions/miniforge3/etc/profile.d/conda.sh" ]; then
+        . "/Users/takumi/.pyenv/versions/miniforge3/etc/profile.d/conda.sh"
     else
-        export PATH="$HOME/.pyenv/versions/miniforge3/bin:$PATH"
+        export PATH="/Users/takumi/.pyenv/versions/miniforge3/bin:$PATH"
     fi
 fi
 unset __conda_setup

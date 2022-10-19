4af9d172cbec22cb4aa60fab1acc5d589e045f00
TakumiO <18618624+TakumiO@users.noreply.github.com>
2022-10-19 15:10:52 +0900

    fix config.zsh

--- .zsh/3config.zsh
+++ .zsh/3config.zsh
@@ -4,11 +4,12 @@ export LDFLAGS="-L/opt/homebrew/opt/curl/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 export LANG=ja_JP.UTF-8
-export HISTFILE=$ZSH_DIR/.zsh_history
-export SHELDON_CONFIG_DIR=$HOME/dotfiles/.sheldon
-export SHELDON_DATA_DIR=$HOME/dotfiles/.sheldon
+export HISTFILE="$ZSH_DIR/.zsh_history"
+export SHELDON_CONFIG_DIR="$HOME/dotfiles/.sheldon"
+export SHELDON_DATA_DIR="$HOME/dotfiles/.sheldon"
 export DOT_DIR="$HOME/dotfiles"
 export HOMEBREW_GIT_PATH="/home/linuxbrew/.linuxbrew/bin/git"
+export BAT_CONFIG_PATH="$HOME/dotfiles/bat/bat.conf"
 
 # 設定
 bindkey -e
@@ -26,3 +27,7 @@ eval "$(rbenv init -)"
 eval "$(pyenv init -)"
 # sheldon autoload
 eval "$(sheldon source)"
+
+# set man pager
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export BAT_PAGER="less -RF"

e38041fde26b79370e23759bd91141d36181c516
Takumi ONO <18618624+TakumiO@users.noreply.github.com>
2023-04-10 13:06:19 +0900

    update config for zsh

--- .zsh/1path.zsh
+++ .zsh/1path.zsh
@@ -1,7 +1,7 @@
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/opt/homebrew/Celler:$PATH"
-export PATH="/opt/homebrew/opt/curl/bin:$PATH"
-export PATH="/opt/homebrew/sbin:$PATH"
+export PATH="$(brew --prefix)/bin:$PATH"
+export PATH="$(brew --prefix)/Celler:$PATH"
+export PATH="$(brew --prefix)/opt/curl/bin:$PATH"
+export PATH="$(brew --prefix)/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 
 
@@ -9,3 +9,7 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.pyenv/shims:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
 export PATH="$HOME/.nodenv/shims:$PATH"
+export PATH="/usr/local/texlive/2023/bin/universal-darwin:$PATH"
+
+export MANPATH="/usr/local/texlive/2023/texmf-dist/doc/man:$MANPATH"
+export INFOPATH="/usr/local/texlive/2023/texmf-dist/doc/info:$INFOPATH"

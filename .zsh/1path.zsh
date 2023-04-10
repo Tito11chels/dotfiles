8f7aad55e9c5fc1e4be101af696085ae6d51d8e9
Takumi <taku.marine@icloud.com>
2023-04-10 15:26:59 +0900

    update configs

--- .zsh/1path.zsh
+++ .zsh/1path.zsh
@@ -9,7 +9,7 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.pyenv/shims:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
 export PATH="$HOME/.nodenv/shims:$PATH"
-export PATH="/usr/local/texlive/2023/bin/universal-darwin:$PATH"
+export PATH="/usr/local/texlive/2023/bin/*:$PATH"
 
 export MANPATH="/usr/local/texlive/2023/texmf-dist/doc/man:$MANPATH"
 export INFOPATH="/usr/local/texlive/2023/texmf-dist/doc/info:$INFOPATH"

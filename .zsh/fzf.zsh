114ef9f3cfc2497ce332c072d0c0e473d792ec34
Takumi ONO <18618624+TakumiO@users.noreply.github.com>
2023-04-12 10:20:09 +0900

    fix config

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -6,10 +6,10 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$(brew --prefix)/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$(brew --prefix)/Cellar/fzf/0.39.0/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$(brew --prefix)/fzf/shell/key-bindings.zsh"
+source "$(brew --prefix)/Cellar/fzf/0.39.0/shell/key-bindings.zsh"
 
 export FZF_DEFAULT_OPTS='--preview "bat --color=always --style=numbers --line-range :500 {}"'

6fbb9d6fc39b16eb53f43860788ee1a316a68414
Takumi ONO <18618624+TakumiO@users.noreply.github.com>
2023-04-12 10:20:09 +0900

    fix config

--- .zsh/1path.zsh
+++ .zsh/1path.zsh
@@ -9,7 +9,12 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.pyenv/shims:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
 export PATH="$HOME/.nodenv/shims:$PATH"
-export PATH="/usr/local/texlive/2023/bin/*:$PATH"
+# if macOS, then set tlmgr
+if [ "$(uname)" = 'Darwin' ]; then
+    export PATH="/usr/local/texlive/2023/bin/universal-darwin/:$PATH"
+else
+    export PATH="/usr/local/texlive/2023/bin/x86_64-linux/:$PATH"
+fi
 
 export MANPATH="/usr/local/texlive/2023/texmf-dist/doc/man:$MANPATH"
 export INFOPATH="/usr/local/texlive/2023/texmf-dist/doc/info:$INFOPATH"

85f41abde0cf6c39a76c59e209c2ccecf831435c
Takumi <taku.marine@icloud.com>
2023-05-09 08:50:00 +0900

    update local::lib

--- .zsh/3config.zsh
+++ .zsh/3config.zsh
@@ -31,3 +31,6 @@ eval "$(sheldon source)"
 # set man pager
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export BAT_PAGER="less -RF"
+
+# set local::lib
+eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"

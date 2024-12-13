f29b78ca7511e20a920437c04c8045af3c625649
Didier <taku.marine@icloud.com>
2024-12-13 22:33:20 +0900

    update comp

--- .zsh/4comp.zsh
+++ .zsh/4comp.zsh
@@ -10,6 +10,7 @@ zstyle ':completion:*:descriptions' format '%F{YELLOW}completing %B%d%b'$DEFAULT
 zstyle ':completion:*:options' description 'yes'
 zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'$DEFAULT
 ## マッチ種別を別々に表示
+zstyle ':completion:*:*:kubectl:*' list-grouped false
 zstyle ':completion:*' group-name ''
 ## セパレータを設定する
 zstyle ':completion:*' list-separator '-->'
@@ -21,3 +22,6 @@ colors
 export LS_COLORS='di=34:ln=40:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 ## ファイル補完候補に色を付ける
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+setopt list_types            # 補完候補一覧でファイルの種別を識別マーク表示 (訳注:ls -F の記号)
+setopt magic_equal_subst     # コマンドラインの引数で --prefix=/usr などの = 以降でも補完できる
